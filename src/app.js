2a8c8a37dd6137e1ddf4a61711f0090f25c859ea
Miguel Angel Lopez Hermida <lacarricanta@hotmail.com>
2025-11-06 16:24:32 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+  function excusaAleatoria(params) {
+
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+    let indiceWho = Math.floor(Math.random() * who.length);
+    let indiceAction = Math.floor(Math.random() * action.length);
+    let indiceWhat = Math.floor(Math.random() * what.length);
+    let indiceWhen = Math.floor(Math.random() * when.length);
+
+    return who[indiceWho] + ' ' + action[indiceAction] + ' ' + what[indiceWhat] + ' ' + when[indiceWhen];
+  }
+
+  document.getElementById("excuse").innerText = excusaAleatoria()
+
 };
+
+window.onload()
+
