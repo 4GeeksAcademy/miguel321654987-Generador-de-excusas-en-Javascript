33ec5a46a4dc4f4d4378a1a80beb204d3aab4558
Miguel Angel Lopez Hermida <lacarricanta@hotmail.com>
2025-11-10 18:38:15 +0000

    Second commit

--- src/app.js
+++ src/app.js
@@ -6,24 +6,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function () {
 
-  function excusaAleatoria(params) {
-
+  function genExcusaAleatoria(params) {
     let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
     let action = ['ate', 'peed', 'crushed', 'broke'];
     let what = ['my homework', 'my phone', 'the car'];
     let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-    let indiceWho = Math.floor(Math.random() * who.length);
-    let indiceAction = Math.floor(Math.random() * action.length);
-    let indiceWhat = Math.floor(Math.random() * what.length);
-    let indiceWhen = Math.floor(Math.random() * when.length);
+    let numberWho = Math.floor(Math.random() * who.length);
+    let numberAction = Math.floor(Math.random() * action.length);
+    let numbereWhat = Math.floor(Math.random() * what.length);
+    let numberWhen = Math.floor(Math.random() * when.length);
 
-    return who[indiceWho] + ' ' + action[indiceAction] + ' ' + what[indiceWhat] + ' ' + when[indiceWhen];
+    return who[numberWho] + ' ' + action[numberAction] + ' ' + what[numbereWhat] + ' ' + when[numberWhen];
   }
-
-  document.getElementById("excuse").innerText = excusaAleatoria()
-
+  document.getElementById("excusa").innerText = genExcusaAleatoria()
 };
-
 window.onload()
 
